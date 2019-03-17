94b7e3f31643600548ef87642e6f5a1e9cd6d0a5
Mitonios <nheo0510@gmail.com>
2019-03-18 02:05:55 +0700

    first commit

---
+++ DiabloIIIMarco/DiabloIIIMarcoDlg.cpp
@@ -0,0 +1,192 @@
+
+// DiabloIIIMarcoDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "DiabloIIIMarco.h"
+#include "DiabloIIIMarcoDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+struct MyConfig
+{
+	int		leftMouseTime;
+	int		rightMouseTime;
+	int		skillSlot01Time;
+	int		skillSlot02Time;
+	int		skillSlot03Time;
+	int		skillSlot04Time;
+	int		skill01Enable;
+	int		skill02Enable;
+	int		skill03Enable;
+	int		skill04Enable;
+};
+MyConfig myConfig;
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDiabloIIIMarcoDlg dialog
+
+
+
+CDiabloIIIMarcoDlg::CDiabloIIIMarcoDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIABLOIIIMARCO_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CDiabloIIIMarcoDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CDiabloIIIMarcoDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CDiabloIIIMarcoDlg message handlers
+
+BOOL CDiabloIIIMarcoDlg::OnInitDialog()
+{
+	// Create mutex
+	HANDLE hMutex = CreateMutex(NULL, TRUE, L"Diablo III Marco");
+	switch (GetLastError())
+	{
+	case ERROR_SUCCESS:
+		// Mutex created successfully. There is 
+		// no instances running
+		break;
+
+	case ERROR_ALREADY_EXISTS:
+		// Mutex already exists so there is a 
+		// running instance of our app.
+		hMutex = NULL;
+		exit(EXIT_SUCCESS);
+		break;
+
+	default:
+		// Failed to create mutex by unknown reason
+		break;
+	}
+
+	srand((unsigned int)time((time_t*)0));
+
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	((CListBox*)GetDlgItem(IDC_LIST_LOG))->InsertString(0, L"Init Program");
+	GetDlgItem(IDD_EDIT_LEFTMOUSE_MS)->SetWindowText(L"100");
+
+	// TODO: Add extra initialization here
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CDiabloIIIMarcoDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CDiabloIIIMarcoDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CDiabloIIIMarcoDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ DiabloIIIMarco/Resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DiabloIIIMarco.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DIABLOIIIMARCO_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_STATIC_RIGHTMOUSE           1000
+#define IDD_EDIT_LEFTMOUSE_MS           1001
+#define IDD_EDIT_RIGHTMOUSE_MS          1002
+#define IDC_STATIC_LEFTMOUSE            1003
+#define IDC_STATIC_AUTOBUTTON           1004
+#define IDD_EDIT_SKILL1                 1005
+#define IDC_CHECK_SKILL1                1006
+#define IDD_EDIT_SKILL2                 1007
+#define IDC_CHECK_SKILL2                1008
+#define IDD_EDIT_SKILL3                 1011
+#define IDC_CHECK_SKILL3                1012
+#define IDD_EDIT_SKILL4                 1013
+#define IDC_CHECK_SKILL4                1014
+#define IDC_LIST_LOG                    1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
